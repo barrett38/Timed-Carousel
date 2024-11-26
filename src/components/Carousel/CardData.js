48aa3ae7ceecc1761873e0b84d6212eeb2a233da
Barrett Anderson <barrett1614@gmail.com>
2024-11-26 12:44:08 -0600

    Setting up Longform

--- src/components/Carousel/CardData.js
+++ src/components/Carousel/CardData.js
@@ -4,42 +4,51 @@ const cardData = [
     title: "Card 1",
     content: "This is the content of card 1.",
     image: "/assets/image1.jpg", // Path to the image
+    longform: "",
   },
   {
     id: 2,
     title: "Card 2",
     content: "This is the content of card 2.",
     image: "/assets/image2.jpg", // Path to the image
+    longform: "",
   },
   {
     id: 3,
     title: "Card 3",
     content: "This is the content of card 3.",
     image: "/assets/image3.jpg", // Path to the image
+    longform: "",
   },
   {
     id: 4,
     title: "Card 4",
     content: "This is the content of card 4.",
     image: "/assets/image4.jpg", // Path to the image
+    longform: "",
   },
   {
     id: 5,
-    title: "Card 5",
+    title: "The Great Pyramid",
     content: "This is the content of card 5.",
     image: "/assets/image5.jpg", // Path to the image
+    longform:
+      "The Great Pyramid of Giza is the only surviving wonder of the ancient world. Built around 2580–2560 BC, it served as a tomb for the Egyptian Pharaoh Khufu (Cheops). Standing at an original height of 146.6 meters (481 feet), it was the tallest man-made structure in the world for over 3,800 years. The pyramid’s precise construction, using massive stone blocks, remains an engineering marvel even today, sparking awe and intrigue over the methods used to build it.",
   },
   {
     id: 6,
-    title: "Card 6",
+    title: "The Hanging Gardens of Babylon",
     content: "This is the content of card 6.",
     image: "/assets/image6.jpg", // Path to the image
+    longform:
+      "The Hanging Gardens, said to be built by King Nebuchadnezzar II around 600 BC, were a legendary terraced garden filled with lush vegetation. According to ancient writers, the gardens were created to please the king’s wife, Amytis, who missed the greenery of her homeland. The gardens were reportedly irrigated by an advanced system using the nearby Euphrates River. However, the existence of the Hanging Gardens remains uncertain, as no definitive archaeological evidence has been found.",
   },
   {
     id: 7,
     title: "Card 7",
     content: "This is the content of card 7.",
     image: "/assets/image7.jpg", // Path to the image
+    longform: "",
   },
 ];
 
