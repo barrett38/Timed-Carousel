19e1c29e0365cd69ded03d0a7cd9a86a8d295f7e
Barrett Anderson <barrett1614@gmail.com>
2024-11-26 11:05:58 -0600

    Separated CardData

---
+++ src/components/Carousel/CardData.js
@@ -0,0 +1,46 @@
+const cardData = [
+  {
+    id: 1,
+    title: "Card 1",
+    content: "This is the content of card 1.",
+    image: "/assets/image1.jpg", // Path to the image
+  },
+  {
+    id: 2,
+    title: "Card 2",
+    content: "This is the content of card 2.",
+    image: "/assets/image2.jpg", // Path to the image
+  },
+  {
+    id: 3,
+    title: "Card 3",
+    content: "This is the content of card 3.",
+    image: "/assets/image3.jpg", // Path to the image
+  },
+  {
+    id: 4,
+    title: "Card 4",
+    content: "This is the content of card 4.",
+    image: "/assets/image4.jpg", // Path to the image
+  },
+  {
+    id: 5,
+    title: "Card 5",
+    content: "This is the content of card 5.",
+    image: "/assets/image5.jpg", // Path to the image
+  },
+  {
+    id: 6,
+    title: "Card 6",
+    content: "This is the content of card 6.",
+    image: "/assets/image6.jpg", // Path to the image
+  },
+  {
+    id: 7,
+    title: "Card 7",
+    content: "This is the content of card 7.",
+    image: "/assets/image7.jpg", // Path to the image
+  },
+];
+
+export default cardData;
